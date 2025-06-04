f261a03bd345f5a47d75b2f808e0d7d514d941b8
Eneko Anguiano <enekoanguianov@alumni.mondragon.edu>
2025-06-04 10:33:45 +0200

    commiteo el script proporcionado

--- ExamenReto4.R
+++ ExamenReto4.R
@@ -9,3 +9,17 @@ library(readxl)
 library(smoothmest)
 library(VIM)
 library(plumber)
+
+datos <- read.csv("C:/Users/nenek/Downloads/netflix.csv")
+
+summary(datos$Minutes)
+summary(datos$Seasons)
+
+length(unique(datos$type))
+length(unique(datos$country))
+length(unique(datos$director))
+
+miss_var_summary(datos)
+vis_miss(datos)
+
+datos[is.na(datos)] <- 0
